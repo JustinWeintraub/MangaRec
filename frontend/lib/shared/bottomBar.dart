83d47338f325b8faf2fbcb5b063b984339a4aeb1
Yoshterra1 <44651405+Yoshterra1@users.noreply.github.com>
2021-01-13 14:00:20 -0500

    Added logout and search

--- frontend/lib/shared/bottomBar.dart
+++ frontend/lib/shared/bottomBar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/screens/manga/mangaWrapper.dart';
 import 'package:frontend/screens/profile.dart';
 import 'package:frontend/screens/recommend.dart';
 import 'package:frontend/shared/contexedRoute.dart';
@@ -20,7 +21,19 @@ Widget bottomBar(context) {
             icon: IconButton(
                 icon: Icon(Icons.list),
                 onPressed: () => Navigator.push(
-                    context, contexedRoute(context, Recommend()))))
+                    context, contexedRoute(context, Recommend())))),
+        BottomNavigationBarItem(
+            label: 'Search',
+            icon: IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () => Navigator.push(
+                    context, contexedRoute(context, MangaWrapper())))),
+        BottomNavigationBarItem(
+            label: 'Logout',
+            icon: IconButton(
+                icon: Icon(Icons.logout),
+                onPressed: () => Navigator.pushNamedAndRemoveUntil(
+                    context, '/', (_) => false)))
       ]);
 
   //userInfo.updateChild(userInfo, MangaViewer(mangaSelected))
